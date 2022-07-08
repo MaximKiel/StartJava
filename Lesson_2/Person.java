48c8d8a8b388c43281144a7f2b888891929f5acc
MaximKiel <loup.du.nord@yandex.ru>
2022-07-08 08:28:32 +0300

    implement Lesson_2 HW_1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    String gender = "мужской";
+    String name = "Макс";
+    int height = 187;
+    double weight = 80.5;
+    int age = 24;
+
+    void walk() {
+        System.out.println(name + " идет");
+    }
+
+    void sit() {
+        System.out.println(name + " сидит");
+    }
+
+    void run() {
+        System.out.println(name + " бежит");
+    }
+
+    void speak() {
+        System.out.println(name + " говорит");
+    }
+
+    void learnJava() {
+        System.out.println(name + " изучает Java");
+    }
+}
