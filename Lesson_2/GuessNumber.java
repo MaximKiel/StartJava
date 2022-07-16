18576d1016e0d0a2fad9f7ec03fcb3a925d499ee
MaximKiel <loup.du.nord@yandex.ru>
2022-07-16 21:27:01 +0300

    edit Lesson_2 HW_final

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,13 +1,11 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
 
     Scanner scanner = new Scanner(System.in);
-    Random random = new Random();
     private Player playerOne;
     private Player playerTwo;
-    private int computerNumber;
+    private int targetNumber;
 
     public GuessNumber(Player player1, Player player2) {
         this.playerOne = player1;
@@ -15,7 +13,7 @@ public class GuessNumber {
     }
 
     public void start() {
-        computerNumber = random.nextInt(100) + 1;
+        targetNumber = (int) (Math.random() * 100) + 1;
         System.out.println("\nНачинаем! Компьютер загадал число от 0 до 100! Теперь попробуйте угадать его!");
         do {
             System.out.print("Игрок " + playerOne.getName() + " предлагает число: ");
@@ -35,17 +33,16 @@ public class GuessNumber {
         if (player.getNumber() <= 0 || player.getNumber() > 100) {
             System.out.println("Ошибка! Число должно быть в полуинтервале (0; 100]");
             return false;
-        } else {
-            if (player.getNumber() == computerNumber) {
-                System.out.println("Игрок " + player.getName() + " угадал!");
-                return true;
-            } else if (player.getNumber() > computerNumber) {
-                System.out.println("Число " + player.getNumber() + " больше того, что загадал компьютер");
-                return false;
-            } else {
-                System.out.println("Число " + player.getNumber() + " меньше того, что загадал компьютер");
-                return false;
-            }
         }
+        if (player.getNumber() == targetNumber) {
+            System.out.println("Игрок " + player.getName() + " угадал!");
+            return true;
+        }
+        if (player.getNumber() > targetNumber) {
+            System.out.println("Число " + player.getNumber() + " больше того, что загадал компьютер");
+        } else {
+            System.out.println("Число " + player.getNumber() + " меньше того, что загадал компьютер");
+        }
+        return false;
     }
 }
