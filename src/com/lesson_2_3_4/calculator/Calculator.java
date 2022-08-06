5a1e16a31438e82e66893547dcdbb58511c8c77b
MaximKiel <loup.du.nord@yandex.ru>
2022-08-06 14:39:14 +0300

    implement lesson_4 HW_final

--- src/com/lesson_2_3_4/calculator/Calculator.java
+++ src/com/lesson_2_3_4/calculator/Calculator.java
@@ -2,55 +2,17 @@ package com.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    private int num1;
-    private int num2;
-    private char mathSign;
-    private int result;
+    private static int result;
 
-    public int getNum1() {
-        return num1;
-    }
-
-    public void setNum1(int num1) {
-        this.num1 = num1;
-    }
-
-    public int getNum2() {
-        return num2;
-    }
-
-    public void setNum2(int num2) {
-        this.num2 = num2;
-    }
-
-    public void setMathSign(char mathSign) {
-        this.mathSign = mathSign;
-    }
-
-    public void calculate() {
+    public static String calculate(int num1, String mathSign, int num2) {
         switch (mathSign) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                result = num1 / num2;
-                break;
-            case '^':
-                result = num1;
-                for (int i = 1; i < num2; i++) {
-                    result *= num1;
-                }
-                break;
-            case '%':
-                result = num1 % num2;
-                break;
-            }
-        System.out.println(num1 + " " + mathSign + " " + num2 + " = " + result);
+            case "+" -> result = num1 + num2;
+            case "-" -> result = num1 - num2;
+            case "*" -> result = num1 * num2;
+            case "/" -> result = num1 / num2;
+            case "^" -> result = (int) Math.pow(num1, num2);
+            case "%" -> result = num1 % num2;
+        }
+        return num1 + " " + mathSign + " " + num2 + " = " + result;
     }
 }
--- src/com/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,26 +1,43 @@
 package com.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
+import static com.lesson_2_3_4.calculator.Calculator.calculate;
 
 public class CalculatorTest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator();
         String answer;
+        String mathExp;
+        String[] mathExpAr;
+        boolean repeat;
 
         do {
-            System.out.print("Введите первое число: ");
-            calculator.setNum1(scanner.nextInt());
-            System.out.print("Введите знак математической операции: ");
-            calculator.setMathSign(scanner.next().charAt(0));
-            System.out.print("Введите второе число: ");
-            calculator.setNum2(scanner.nextInt());
+            do {
+                try {
+                    System.out.print("Введите математическое выражение: ");
+                    mathExp = scanner.nextInt() + " " + scanner.next().charAt(0) + " " + scanner.nextInt();
+                    mathExpAr = mathExp.split(" ");
 
-            calculator.calculate();
+                    if (Integer.parseInt(mathExpAr[0]) > 0 && Integer.parseInt(mathExpAr[2]) > 0) {
+                        System.out.println(calculate(Integer.parseInt(mathExpAr[0]),
+                                mathExpAr[1], Integer.parseInt(mathExpAr[2])));
+                        repeat = false;
+                    } else {
+                        System.out.println("Числа для вычислений должны быть положительными!");
+                        repeat = true;
+                    }
+                }
+                catch (Throwable exc) {
+                    System.out.println("Вы ввели некорректное выражение!");
+                    repeat = true;
+                    scanner.next();
+                }
+
+            } while (repeat);
 
             do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]:");
+                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
                 answer = scanner.next();
             } while (!answer.equals("yes") && !answer.equals("no"));
         } while (answer.equals("yes"));
