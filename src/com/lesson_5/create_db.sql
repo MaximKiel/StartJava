a06c23c170b0e31fea98957704fdef3d3e4db31e
MaximKiel <loup.du.nord@yandex.ru>
2022-09-05 20:18:19 +0300

    edit HW_lesson_5

--- src/com/lesson_5/create_db.sql
+++ src/com/lesson_5/create_db.sql
@@ -1,12 +1,15 @@
 CREATE DATABASE jaegers;
+
 \c jaegers;
-   CREATE TABLE jaegers (
-                id        serial primary key,
-                modelName text,
-                mark      text,
-                height    int,
-                weight    decimal,
-                status    text,
-                origin    text,
-                launch    date,
-                kaijuKill int);
+
+CREATE TABLE jaegers (
+id        SERIAL PRIMARY KEY,
+modelName TEXT,
+mark      TEXT,
+height    INT,
+weight    DECIMAL,
+status    TEXT,
+origin    TEXT,
+launch    DATE,
+kaijuKill INT
+);
