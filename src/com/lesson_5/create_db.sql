852688b17190024cc01cbad8a0543e36c56426ba
MaximKiel <loup.du.nord@yandex.ru>
2022-09-05 21:52:39 +0300

    edit HW_lesson_5

--- src/com/lesson_5/create_db.sql
+++ src/com/lesson_5/create_db.sql
@@ -3,13 +3,13 @@ CREATE DATABASE jaegers;
 \c jaegers;
 
 CREATE TABLE jaegers (
-id        SERIAL PRIMARY KEY,
-modelName TEXT,
-mark      TEXT,
-height    INT,
-weight    DECIMAL,
-status    TEXT,
-origin    TEXT,
-launch    DATE,
-kaijuKill INT
+    id        SERIAL PRIMARY KEY,
+    modelName TEXT,
+    mark      TEXT,
+    height    INT,
+    weight    DECIMAL,
+    status    TEXT,
+    origin    TEXT,
+    launch    DATE,
+    kaijuKill INT
 );
